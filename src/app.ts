b77ad443c781bd8dd6d74182d903c4dd73cf03bf
AliRahman06 <4li.ar45@gmail.com>
2021-08-23 14:21:29 +0700

    tambahan

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
+
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
@@ -21,6 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
